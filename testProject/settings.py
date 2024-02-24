984f313c71fd44512ad8552c871c287926bac0ff
mhabib5073 <m.habib5073@gmail.com>
2024-02-24 19:10:14 +0530

    4th_commit

--- testProject/settings.py
+++ testProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-503sjcm_v54xe1oqm^ynr-$(pqto**e5xh!fmud$sd*fw_a$oe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
